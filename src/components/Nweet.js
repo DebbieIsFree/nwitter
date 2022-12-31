f0ba4bc22bd17db99a331e5103f090c57aa2cc78
Debbie <wldnjs980409@naver.com>
2022-12-31 22:35:24 +0900

    0.6 update doc

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,7 +1,12 @@
 import { dbservice } from "fbase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);  // toggle
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const toggleEditing = () => setEditing((prev) => !prev);
 
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
@@ -20,20 +25,55 @@ const Nweet = ({ nweetObj, isOwner }) => {
                     console.log(error);
                 })
         }
-    }
+    };
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewNweet(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const docRef = doc(dbservice, 'nweets/${nweetObj.id}');
+        const data = {
+            text: newNweet,
+            creatorId: nweetObj.creatorId,
+            createdAt: nweetObj.createdAt
+        };
+        setDoc(docRef, data)
+            .then(() => {
+                console.log("The value of nweet is updated!");
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    };
 
     return (
         <div>
-            <h4>{nweetObj.text}</h4>
-            {/*  하위 컴포넌트에서 상위 컴포넌트로부터 넘겨받은 데이터 result를 
-            사용할 때는 props.result의 형태로 사용해야 한다. */}
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button onClick={onDeleteClick}>Delete Nweet</button>
-                    <button>Edit Nweet</button>
+                    <form onSubmit={onSubmit}>
+                        <input onChange={onChange} value={newNweet} required />
+                        <input type="submit" value="Update" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
                 </>
-            )
-            }
+            ) : (
+                <>
+                    <h4>{nweetObj.text}</h4>
+                    {/* 하위 컴포넌트에서 상위 컴포넌트로부터 넘겨받은 데이터 result를
+                    사용할 때는 props.result의 형태로 사용해야 한다.  */}
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Nweet</button>
+                            <button onClick={toggleEditing}>Edit Nweet</button>
+                        </>
+                    )}
+                </>
+            )}
         </div >
     );
 };
